54ceba80ec647bd1836ba6f8bb1b1d9394066734
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2022-07-06 16:02:49 -0300

    feat: ✨Capture Title, Price, Seller and link

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,19 @@ import pup from 'puppeteer';
 const url = "https://www.mercadolivre.com.br/";
 const searchFor = "macbook";
 
+
+interface IResponse {
+    title?: string | null;
+    price?: string | null;
+    seller?: string | null;
+    link?: string | null;
+}
+
+
 (async () => {
-    const browser = await pup.launch({ headless: false });
+    let c = 1;
+    const products: IResponse[] = [];
+    const browser = await pup.launch({ headless: true });
     const page = await browser.newPage();
     console.log("iniciado");
     await page.goto(url);
@@ -13,8 +24,38 @@ const searchFor = "macbook";
 
     await page.type('#cb1-edit', searchFor)
 
-    await page.waitForTimeout(2000)
 
-    console.log("entrei na url");
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('.nav-search-btn')
+    ])
+
+    const links = await page.$$eval('.ui-search-result__image > a', (el) => el.map((link: any) => link.href));
+
+    for (const link of links) {
+        console.log('Pagina ', c);
+        await page.goto(link)
+        await page.waitForSelector('.ui-pdp-title');
+
+        const title = await page.$eval('.ui-pdp-title', el => el.textContent);
+        const price = await page.$eval('.andes-money-amount__fraction', el => el.textContent)
+
+        const seller = await page.evaluate(() => {
+            const el = document.querySelector('.ui-pdp-seller__header__title .ui-pdp-action-modal__link');
+            if (!el) return null;
+            return el.textContent;
+        })
+
+        const obj: IResponse = {
+            title,
+            price,
+            seller: seller ? seller : "",
+            link
+        };
+        products.push(obj)
+        c++;
+    }
+    console.log(products);
+    await page.waitForTimeout(2000)
     await browser.close();
 })();
